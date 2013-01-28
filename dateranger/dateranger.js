9def1a641673a1c6e5a3474847095b7bb1dc7ba6
Laurent Yaish <laurenty@gmail.com>
2013-01-27 22:20:25 -0800

    Set minDate when start and end dates are selected

--- dateranger/dateranger.js
+++ dateranger/dateranger.js
@@ -23,17 +23,16 @@
 						}
 						if ( ! start_date) {
 							start_date = $.datepicker.parseDate('mm/dd/yy', date_text);
+							inst.settings.minDate = start_date;
 							return;
 						}
 						if ( ! end_date) {
 							end_date = $.datepicker.parseDate('mm/dd/yy', date_text);
+							inst.settings.minDate = null;
 							return;
 						}
 					},
 					beforeShowDay: function(date) {
-						if (date < start_date && ! end_date) {
-							return [false];
-						}
 						if (date >= start_date && date <= end_date) {
 							return [true, 'red'];
 						}
