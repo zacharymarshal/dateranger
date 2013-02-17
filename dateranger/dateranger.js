ded9c5ce7a6bfb3e8ae82af16d79991240a40e3c
Laurent Yaish <laurenty@gmail.com>
2013-02-17 10:25:59 -0800

    Code updates inspired by the widget factory right practices

--- dateranger/dateranger.js
+++ dateranger/dateranger.js
@@ -3,51 +3,27 @@
  
 		// Default options
 		options: { 
-			defaultStartDate: new Date(),
-			defaultEndDate: new Date(),
-			onSelectStartDate: null,
-			onSelectEndDate: null
+			start_date: new Date(),
+			end_date: new Date(),
+			numberOfMonths: 3,
+			onSelectStartDate: function(e, obj) {},
+			onSelectEndDate: function(e, obj) {}
 		},
 
-		start_date: null,
-		end_date: null,
-		date_picker: null,
+		_datepicker: null,
 	 
 		// Set up the widget
 		_create: function() {
 			var self = this;
 
-			self.date_picker = self.element.datepicker({
-				numberOfMonths: 3,
+			self._datepicker = self.element.datepicker({
+				numberOfMonths: self.options.numberOfMonths,
 				showButtonPanel: false,
 				onSelect: function(date_text, inst) {
-					if (self.start_date && self.end_date) {
-						self.start_date = false;
-						self.end_date = false;
-					}
-					if ( ! self.start_date) {
-						self.start_date = $.datepicker.parseDate('mm/dd/yy', date_text);
-
-						if (self.options.onSelectStartDate) {
-							self.options.onSelectStartDate(self.start_date);
-						}
-
-						inst.settings.minDate = self.start_date;
-						return;
-					}
-					if ( ! self.end_date) {
-						self.end_date = $.datepicker.parseDate('mm/dd/yy', date_text);
-
-						if (self.options.onSelectEndDate) {
-							self.options.onSelectEndDate(self.end_date);	
-						}
-						
-						inst.settings.minDate = null;
-						return;
-					}
+					self._onSelect(date_text, inst);
 				},
 				beforeShowDay: function(date) {
-					if (date >= self.start_date && date <= self.end_date) {
+					if (date >= self.options.start_date && date <= self.options.end_date) {
 						return [true, 'ui-dateranger-selected'];
 					} else {
 						return [true, ''];
@@ -58,11 +34,19 @@
 	 
 		// Use the _setOption method to respond to changes to options
 		_setOption: function( key, value ) {
+
 			switch( key ) {
-			case "defaultStartDate":
-				break;
-			case "defaultEndDate":
-				break;
+				case "start_date":
+					value = $.datepicker.parseDate('mm/dd/yy', value);
+					this._datepicker.minDate = value;
+					this._trigger('onSelectStartDate', null, {date_text: value, inst: this._datepicker});
+					break;
+
+				case "end_date":
+					value = $.datepicker.parseDate('mm/dd/yy', value);
+					this._datepicker.minDate = null;
+					this._trigger('onSelectEndDate', null, {date_text: value, inst: this._datepicker});
+					break;
 			}
 	 
 			// In jQuery UI 1.8, you have to manually invoke the _setOption method from the base widget
@@ -71,12 +55,30 @@
 			this._super( "_setOption", key, value );
 		},
 
+
+		_onSelect: function(date_text, inst) {
+
+			if (this.options.start_date && this.options.end_date) {
+				this.options.start_date = false;
+				this.options.end_date = false;
+			}
+			if ( ! this.options.start_date) {
+				this.setStartDate(date_text);
+				return;
+			}
+			if ( ! this.options.end_date) {
+				this.setEndDate(date_text);
+				return;
+			}
+		},
+
+
 		setStartDate: function(date_text) {
-			return this.datepicker._selectDate('#' + $(this).attr('id'), date_text);
+			this._setOption('start_date', date_text);
 		},
 
 		setEndDate: function(date_text) {
-			return this.datepicker._selectDate('#' + $(this).attr('id'), date_text);
+			this._setOption('end_date', date_text);
 		},
 
 	 
@@ -85,6 +87,10 @@
 			// In jQuery UI 1.8, you must invoke the destroy method from the base widget
 			$.Widget.prototype.destroy.call( this );
 			// In jQuery UI 1.9 and above, you would define _destroy instead of destroy and not call the base method
+		},
+
+		_constraintDate: function(date) {
+
 		}
 	});
 }( jQuery ) );
